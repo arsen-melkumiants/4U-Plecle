ac1a01af9c01364d5768f0de30803cc6e9e282cc
Arsen Melkumiants <arsen.mem@gmail.com>
2014-07-08 00:11:51 +0300

    update client status

--- application/views/orders/client_top.php
+++ application/views/orders/client_top.php
@@ -40,7 +40,7 @@
 				<?php } elseif ($order_info['status'] == 3) {?>
 					<div class="big_status">Уборка завершена</div>
 					<div class="add_title"><?php echo date('В H:i d.m.Y', $order_info['start_date'] + (3600 * $order_info['duration']))?></div>
-				<?php } elseif ($order_info['status'] == 3) {?>
+				<?php } elseif ($order_info['status'] > 3) {?>
 					<div class="big_status">Сделка отменена</div>
 					<div class="add_title"><?php echo date('В H:i d.m.Y', $order_info['start_date'] + (3600 * $order_info['duration']))?></div>
 				<?php }?>
