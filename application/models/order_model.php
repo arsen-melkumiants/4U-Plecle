b388bb932a1820c0cf5e3412df937741e052f74e
Arsen Melkumiants <arsen.mem@gmail.com>
2014-07-22 21:08:01 +0300

    fix orders

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -80,7 +80,7 @@ class Order_model extends CI_Model {
 			$this->db->where('(status = 2 OR (status = 1 AND start_date > '.(time() + 86400).'))');
 		} elseif ($status == 3) {
 			$this->db->where('cleaner_id', 0);
-			$this->db->where('((status = 2 AND start_date > '.time().') OR (status IN (0,1) AND start_date > '.(time() + 86400).'))');
+			$this->db->where('((status = 2 AND start_date > '.time().') OR (status IN (1) AND start_date > '.(time() + 86400).'))');
 		} else {
 			$this->db->where('(status > 2 OR (status IN (0,1) AND start_date < '.(time() + 86400).'))');
 		}
