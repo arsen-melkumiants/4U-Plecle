c1087674dba72ebd52ebce31f9b8ecd27d8f7187
Arsen Melkumiants <arsen.mem@gmail.com>
2014-07-22 18:23:19 +0300

    add accept email

---
+++ application/views/email/accept_order.php
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		<h4>Ваша завяка принята</h4>
+		<p>Поздравляем! Завка номер #<?php echo $order_id;?> принята. Уборка состоится <?php echo $start_date?></p>
+		<p>Подробности сделки доступны в вашем <a href="<?php echo site_url('orders/detail/'.$order_id)?>">личном кабинете</a></p>
+		<br />
+		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+	</body>
+</html>
