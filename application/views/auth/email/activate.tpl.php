4f31cf3f7610d941a445e1df024978248af7571b
Iryna Turkina <iryna.turkina@gmail.com>
2014-07-28 22:02:10 +0300

    update activate email

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -14,17 +14,5 @@
 		<br />
 		<br />
 		<?php }?>
-
-
-
-		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'en') { ?>
-		<h4>Account Activation <?php echo $identity;?></h4> 
-		<p>to activate your account please click on the link <?php echo anchor('personal/activate/'.$id.'/'.$activation, 'Activate account');?>.</p> 
-		<p>After clicking on the link you can log in.</p> 
-		<br /> 
-		<p> At impossibility to link higher education - copy the link <?php echo base_url('personal/activate/'.$id.'/'.$activation)?> into your browser yourself. </p>
-		<br />
-		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
-		<?php }?>
 	</body>
 </html>
